c488fb842400ca6c9256d945d56e24c7bb753d1d
Artem <108129792+Langepas94@users.noreply.github.com>
2023-06-21 08:52:24 +0300

    Initial Commit

---
+++ TodoApp_SwiftUI/TodoApp_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoApp_SwiftUIApp.swift
+//  TodoApp_SwiftUI
+//
+//  Created by Artem on 21.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct TodoApp_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
